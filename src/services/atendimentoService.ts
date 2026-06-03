82e59e0a880d4cf3b0552f5412e6abdb28a76365
weslley <weslly.delgado@avmb.com.br>
2026-06-03 09:02:33 -0300

    refatoração service

--- src/services/atendimentoService.ts
+++ src/services/atendimentoService.ts
@@ -1,22 +1,22 @@
 import api from './api';
-import type { Atendimento } from 'src/types/atendimento';
+import type { Atendimento, AtendimentoCreate, AtendimentoUpdate } from 'src/types/atendimento';
 
 export const AtendimentoService = {
-  listar: async () => {
+  listar: async (): Promise<Atendimento[]> => {
     const { data } = await api.get('/atendimentos');
     return data;
   },
 
-  criar: async (atendimento: Atendimento) => {
+  criar: async (atendimento: AtendimentoCreate): Promise<Atendimento> => {
     const { data } = await api.post('/atendimentos', atendimento);
     return data;
   },
 
-  atualizar: async (id: number, atendimento: Atendimento) => {
+  atualizar: async (id: number, atendimento: AtendimentoUpdate): Promise<Atendimento> => {
     const { data } = await api.put(`/atendimentos/${id}`, atendimento);
     return data;
   },
-  remover: async (id: number) => {
+  remover: async (id: number): Promise<void> => {
     const { data } = await api.delete(`/atendimentos/${id}`);
     return data;
   },
